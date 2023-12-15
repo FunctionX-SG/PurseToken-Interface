3a7d0c6894b50e8032658fb309fd0a50a83e1aa6
JianAn30 <siau@pundix.com>
2023-12-15 16:16:21 +0800

    Add claim reward popup at Rewards page

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -7,7 +7,7 @@ export default function Popup(props: any) {
         <div >
             {props.trigger ?
                 <div className="popup">
-                    <div className="popup-inner ml-auto mr-auto" style={{marginTop:"70px"}} >
+                    <div className="popup-inner ml-auto mr-auto" style={{marginTop:"70px", width:`${props.width}`}} >
                             <CloseButton data-bs-theme="dark" aria-label="Hide" onClick={() => {
                                 props.setTrigger(false)
                             }}>close
