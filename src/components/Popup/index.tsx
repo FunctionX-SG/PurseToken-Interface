fa6b7f364a4e49b2f4f92fd6d56d4220132ee0b1
malcolmtan <zherui_tan@live.com.sg>
2024-06-18 15:44:49 +0800

    Update Farm UI and build

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -7,7 +7,14 @@ export default function Popup(props: any) {
         <div >
             {props.trigger ?
                 <div className="popup">
-                    <div className="popup-inner ml-auto mr-auto" style={{marginTop:"70px", width:`${props.width}`}} >
+                    <div className="popup-inner ml-auto mr-auto"
+                         style={{
+                             marginTop:"70px",
+                             width:`${props.width}`,
+                             borderRadius: "10px",
+                             backgroundColor: "rgba(248,248,248,0.85)"
+                        }}
+                    >
                             <CloseButton data-bs-theme="dark" aria-label="Hide" onClick={() => {
                                 props.setTrigger(false)
                             }}>close
